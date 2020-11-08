323f576dc7c81cc4859b52d42373a0c7cf4d1915
Amyr <amyrchelokov@yandex.ru>
2020-11-08 18:36:40 +0300

    commit1

---
+++ src/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div className="content">
+        <img src="https://livingmags.co.uk/wp-content/uploads/2019/07/astronomy.jpg"/>
+    
+          <div>
+            My posts
+            <div>
+              New post
+            </div>
+            <div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+            </div>
+            </div>
+          </div>
+    )
+}
+
+export default Profile
